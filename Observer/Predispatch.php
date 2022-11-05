f190d8982fd1199679c4912e5fcf0450379a43e5
Amjad M <amjadm61@gmail.com>
2022-11-06 02:48:15 +0530

    multiwebsite - url fix

--- Observer/Predispatch.php
+++ Observer/Predispatch.php
@@ -51,50 +51,65 @@ class Predispatch implements ObserverInterface {
         if (!$simpleProduct || $simpleProduct->getTypeId() != \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE) {
             return;
         }
+        try {
+            $childId = $simpleProductId;
+            $parentId = $this->getParentId($childId);
+            if($childId != $parentId) {
+                $configProduct =$this->_productRepository->getById($parentId);
+                $configType = $configProduct->getTypeInstance();
+                $attributes = $configType->getConfigurableAttributesAsArray($configProduct);
 
-        $configProductIds = $this->_productTypeConfigurable->getParentIdsByChild($simpleProductId);
-
-        foreach ($configProductIds as $configProductId) {
-            try {
-                $currentWebsiteId = $this->_storeManager->getStore()->getWebsiteId();
-                $websiteIds = $this->_productRepository->getById($configProductId)->getWebsiteIds();
-                if(in_array($currentWebsiteId, $websiteIds)){
-                    $configProduct =  $this->_productRepository->getById($configProductId);
-                } else {
-                    return;
+                $options = [];
+                foreach ($attributes as $attribute) {
+                    $id = $attribute['attribute_id'];
+                    $value = $simpleProduct->getData($attribute['attribute_code']);
+                    $options[$id] = $value;
                 }
-            } catch (NoSuchEntityException $e) {
-                continue;
+
+                // Pass on any query parameters to the configurable product's URL.
+                $query = $request->getQuery();
+                if (is_object($query)) {
+                    $query = $query->toArray();
+                }
+                $query = $query ? '?' . http_build_query($query) : '';
+
+                // Generate hash for selected product options.
+                $hash = $options ? '#' . http_build_query($options) : '';
+
+                $configProductUrl = $configProduct->getUrlModel()->getUrl($configProduct) . $query . $hash;
+                $this->_redirect->setRedirect($configProductUrl, 301);
+            } else {
+                return;
             }
-
-            $configType = $configProduct->getTypeInstance();
-            $attributes = $configType->getConfigurableAttributesAsArray($configProduct);
-
-            $options = [];
-            foreach ($attributes as $attribute) {
-                $id = $attribute['attribute_id'];
-                $value = $simpleProduct->getData($attribute['attribute_code']);
-                $options[$id] = $value;
-            }
-
-            // Pass on any query parameters to the configurable product's URL.
-            $query = $request->getQuery();
-            if (is_object($query)) {
-                $query = $query->toArray();
-            }
-            $query = $query ? '?' . http_build_query($query) : '';
-
-            // Generate hash for selected product options.
-            $hash = $options ? '#' . http_build_query($options) : '';
-
-            $configProductUrl = $configProduct->getUrlModel()
-                                              ->getUrl($configProduct) . $query . $hash;
-            $this->_redirect->setRedirect($configProductUrl, 301);
+    
+        } catch (NoSuchEntityException $noSuchEntityException) {
+            return;
         }
     }
 
     public function isModuleEnabled() {
         $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
         return $this->scopeConfig->getValue('techyouknowredirectsimpleproducts/general/enable', $storeScope);
+    }
+
+    public function getParentId($childId) {
+        $currentWebsiteId = $this->_storeManager->getStore()->getWebsiteId();
+        $parentProduct = $this->_productTypeConfigurable->getParentIdsByChild($childId);
+        if(count($parentProduct) > 0){
+            foreach ($parentProduct as $parentProductId) {
+                if(isset($parentProductId)){
+                    $websiteIds = $this->_productRepository->getById($parentProductId)->getWebsiteIds();
+                    if(in_array($currentWebsiteId, $websiteIds)){
+                        return $parentProductId;
+                    }
+                }
+            }
+        } else {
+            if(isset($parentIds[0])){
+                return $parentIds[0];
+            }
+        }
+       
+        return $childId;
     }
 }
